0c836d5d3d0111c763e33029304fc7eab009ab2d
Jakub Masek <masekjak02@gmail.com>
2024-11-29 13:43:47 +0100

    Add item picker functionality with item list

---
+++ picker.py
@@ -0,0 +1,27 @@
+import os
+import random
+
+def import_items(file_path):
+    with open(file_path, 'r') as file:
+        items = file.read().splitlines()
+    return items
+
+def pick():
+    origItems = import_items('/home/masek/Projects/pairs-picker/items.txt')
+    isPaired = False
+    while not isPaired:
+        items = origItems.copy()
+        notPaired = origItems.copy()
+        for item in items:
+            picked = random.choice(notPaired)
+            while picked is item:
+                if len(notPaired) == 1:
+                    isPaired = False
+                    break;
+                picked = random.choice(notPaired)
+            print(f"{item} - {picked}")
+            notPaired.remove(picked)
+            isPaired = True
+
+if __name__ == "__main__":
+    pick()
